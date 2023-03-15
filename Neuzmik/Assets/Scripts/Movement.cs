7f984e50c5c4f1ff87049c2f11b484473d80304e
Cartelis <sap.deividas@gmail.com>
2023-03-15 14:02:41 +0200

    pamokos darbas

--- Neuzmik/Assets/Scripts/Movement.cs
+++ Neuzmik/Assets/Scripts/Movement.cs
@@ -7,22 +7,43 @@ public class Movement : MonoBehaviour
     public int Greitis;
     Vector3 Vec;
     public KeyCode dash;
+
+    private float activeMoveSpeed;
+    public float dashSpeed;
+    public float dashLength = .5f, dashCooldown = 1f;
+    private float dashCounter;
+    private float dashCoolCounter;
     void Start()
     {
-
+        activeMoveSpeed = Greitis;
     }
-
     void Update()
     {
         Vec = transform.localPosition;
         Vec.x += Input.GetAxis("Horizontal") * Time.deltaTime * Greitis;
         Vec.y += Input.GetAxis("Vertical") * Time.deltaTime * Greitis;
         transform.localPosition = Vec;
+
         if (Input.GetKeyDown(dash))
         {
-            Vec.x += Input.GetAxis("Horizontal") * 100f;
-            Vec.y += Input.GetAxis("Vertical") * 100f;
-            Debug.Log("Bam!");
+            if(dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+        if(dashCounter > 0){
+            dashCounter -= Time.deltaTime;
+            if(dashCounter <= 0)
+            {
+                activeMoveSpeed = Greitis;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+        if(dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
         }
     }
+ 
 }
