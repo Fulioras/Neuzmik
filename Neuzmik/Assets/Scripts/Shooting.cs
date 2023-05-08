7367e8afe462cdf937416c88371d65be03d83a39
Tautvydas <stonkusesu@gmail.com>
2023-05-08 10:17:07 +0300

    Shooting sutvarkytas

--- Neuzmik/Assets/Scripts/Shooting.cs
+++ Neuzmik/Assets/Scripts/Shooting.cs
@@ -6,11 +6,13 @@ public class Shooting : MonoBehaviour
     private bool isShooting = false;
     public GameObject fireEffect;
     public AudioSource soundEffect;
-    private float timeSinceLastShot = 0.1f;
+    private float timeSinceLastShot = 0.0f;
     private Nustatymai config;
 
+    public Transform gunTipTransform;
+    public Transform armTransform;
 
-        private void Start()
+    private void Start()
     {
         GameObject configObject = GameObject.FindGameObjectWithTag("Nustatymai");
         if (configObject != null)
@@ -26,66 +28,63 @@ public class Shooting : MonoBehaviour
     void Update()
     {
         timeSinceLastShot += Time.deltaTime;
-    if(!EscapeMenu.inEscape){
-if (config.firingMode == 1)
+        if (!EscapeMenu.inEscape)
         {
-            if (Input.GetButtonDown("Fire1") && timeSinceLastShot >= ((1/config.rpm)*60))
+            if (config.firingMode == 1)
             {
-                Shoot();
-                soundEffect.Play();
-                GameObject effect = Instantiate(fireEffect, transform.position, transform.rotation);
-                //effect.transform.parent = transform;
-                timeSinceLastShot = 0f;
-                Destroy(effect, 0.2f);
-                timeSinceLastShot = 0f;
+                if (Input.GetButtonDown("Fire1") && timeSinceLastShot >= (60.0f / config.rpm))
+                {
+                    Shoot();
+                    soundEffect.Play();
+                    GameObject effect = Instantiate(fireEffect, transform.position, transform.rotation);
+                    effect.transform.parent = transform;
+                    Destroy(effect, 0.2f);
+                    timeSinceLastShot = 0.0f;
+                }
             }
-        }
-        else if (config.firingMode == 2)
-        {
-            if (Input.GetButtonDown("Fire1"))
+            else if (config.firingMode == 2)
             {
-                isShooting = true;
-            }
-            else if (Input.GetButtonUp("Fire1"))
-            {
-                isShooting = false;
-            }
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    isShooting = true;
+                }
+                else if (Input.GetButtonUp("Fire1"))
+                {
+                    isShooting = false;
+                }
 
-            if (isShooting && timeSinceLastShot >= ((1/config.rpm)*60))
-            {
-                Shoot();
-                soundEffect.Play();
-                GameObject effect = Instantiate(fireEffect, transform.position, transform.rotation);
-                effect.transform.parent = transform;
-                timeSinceLastShot = 0f;
-                Destroy(effect, 0.2f);
+                if (isShooting && timeSinceLastShot >= (60.0f / config.rpm))
+                {
+                    Shoot();
+                    soundEffect.Play();
+                    GameObject effect = Instantiate(fireEffect, transform.position, transform.rotation);
+                    effect.transform.parent = transform;
+                    Destroy(effect, 0.2f);
+                    timeSinceLastShot = 0.0f;
+                }
             }
         }
     }
+
+    void Shoot()
+    {
+        // Get the hand position in world space
+        Vector3 handPos = armTransform.position;
+
+        // Calculate the direction from the hand to the gun tip
+        Vector3 direction = gunTipTransform.position - handPos;
+        direction.z = 0.0f;
+
+        // Add random deviation to direction to simulate inaccuracy
+        Vector3 deviation = new Vector3(Random.Range(-config.accuracy, config.accuracy), Random.Range(-config.accuracy, config.accuracy), 0.0f);
+        direction += deviation;
+
+        Vector3 bulletSpawn = transform.position;
+        GameObject newBall = Instantiate(ballPrefab, bulletSpawn, transform.rotation);
+        Rigidbody2D bulletRb = newBall.GetComponent<Rigidbody2D>();
+        bulletRb.velocity = direction.normalized * config.ballSpeed;
+
+        // Destroy the bullet after a certain time to prevent it from living forever
+        Destroy(newBall, 0.4f);
     }
-public Transform gunTipTransform;
-public Transform armTransform;
-   void Shoot()
-{
-     // Get the hand position in world space
-    Vector3 handPos = armTransform.position;
-
-    // Calculate the direction from the hand to the gun tip
-    Vector3 direction = gunTipTransform.position - handPos;
-    direction.z = 0f;
-
-
-
-    // Add random deviation to direction to simulate inaccuracy
-    Vector3 deviation = new Vector3(Random.Range(-config.accuracy, config.accuracy), Random.Range(-config.accuracy, config.accuracy), 0f);
-    direction += deviation;
-
-    Vector3 bulletSpawn = transform.position;
-    GameObject newBall = Instantiate(ballPrefab, bulletSpawn, transform.rotation);
-    Rigidbody2D bulletRb = newBall.GetComponent<Rigidbody2D>();
-    bulletRb.velocity = direction.normalized * config.ballSpeed;
-
-    // Destroy the bullet after a certain time to prevent it from living forever
-    Destroy(newBall, 0.4f);
-}
 }
